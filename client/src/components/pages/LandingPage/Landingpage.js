77e0ac42fdebb59dab6b3b2ff609164f7b22144c
kangdari_ <ksh44820@naver.com>
2020-07-17 20:03:44 +0900

    add logout event

--- client/src/components/pages/LandingPage/Landingpage.js
+++ client/src/components/pages/LandingPage/Landingpage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
-const Landingpage = () => {
-  return <div>Landingpage</div>;
+const Landingpage = ({ history }) => {
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((res) => {
+      if (res.data.logoutSuccess) {
+        history.push('/login');
+      } else {
+        alert('logout 실패');
+      }
+    });
+  };
+
+  return (
+    <div>
+      <h2>Landingpage</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
 };
 
 export default Landingpage;
